ddb28db901aa1ceeb826cd95d57b2287c78da554
Caio Barbieri <89612905+caio-o@users.noreply.github.com>
2026-04-29 19:21:29 -0400

    Adicionar openpyxl às dependencias.

--- Instalar_dependencia.bat
+++ Instalar_dependencia.bat
@@ -2,7 +2,7 @@
 cd /d "%~dp0"
 
 echo Instalando dependencias...
-py -m pip install -q opencv-python pillow pytesseract pynput numpy python-docx
+py -m pip install -q opencv-python pillow pytesseract pynput numpy python-docx openpyxl
 
 echo Iniciando app...
 py app_word_ocr.py
